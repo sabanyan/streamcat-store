ee8c6c1c1f6996e166fa7aafcff64006a18458f9
yasuyuki okasaka <yasuyuki.okasaka@ksk-anl.com>
2019-07-01 11:16:39 +0900

    fix Utf8ToCp932Command

--- kskp/store/commands/pcmd/script.py
+++ kskp/store/commands/pcmd/script.py
@@ -253,21 +253,20 @@ class Utf8ToCp932Command(PCommand):
         super().__init__()
 
     def run(self, args, inputs):
-        def Cp932_to_utf8():
+        def utf8_to_Cp932():
             """
-            ストリームでcp932→utf8に変換するコマンド
+            ストリームでutf-8→cp932に変換するコマンド
             """
             import sys
             import traceback
             import io
 
             try:
-                # stdinのencodingがデフォルトでutf-8なので、設定し直す。
-                input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='cp932')
-                with open('result.csv', 'w') as f:
-                    for line in input_stream:
-                        # 標準出力するときも自動でutf-8に変換されるので、printだけでいい
-                        print(line, end='')
+                sys.stdout.flush()
+                sys.stdout = open(sys.stdout.fileno(), 'w', encoding='cp932', closefd=False)
+                for line in sys.stdin:
+                    # 改行コードは変えてくれなさそうなのでここで変える
+                    print(line.strip() + '\r\n', end='')
             except Exception as e:
                 with open('/dev/stderr', 'w') as fpe:
                     traceback.print_exc(file=fpe)
