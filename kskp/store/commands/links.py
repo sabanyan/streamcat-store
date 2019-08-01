fc0c8899ed21f970ba24961dbced51d0da664ce1
yasuyuki okasaka <yasuyuki.okasaka@ksk-anl.com>
2019-08-01 09:40:02 +0900

    add demo visualize

--- kskp/store/commands/links.py
+++ kskp/store/commands/links.py
@@ -130,6 +130,10 @@ class CommandLink:
             'csvtohistogram': CsvToHistogramCommand(),
             'csvtoscatter': CsvToScatterCommand(),
             'csvtoboxplot': CsvToBoxplotCommand(),
+            'demo_csvtohatching': CsvToHatching(),
+            'demo_csvtoheatmap': CsvToHeatmap(),
+            'demo_csvtowaveform': CsvToWaveForm(),
+            'demo_rangetool': RangeTool(),
             # Storeコマンド
             'saver': SaverCommand(),
             'cachesaver': CacheSaverCommand(),
