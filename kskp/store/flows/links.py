0352b207a1a0d854377b3782292949e56ef92c8a
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2019-09-18 16:59:07 +0900

    Add resolve_label func to the FlowLink
    # Conflicts:
    #	kskp/store/flows/links.py

--- kskp/store/flows/links.py
+++ kskp/store/flows/links.py
@@ -4,11 +4,19 @@ from pathlib import Path
 class FlowLink:
     def __init__(self, flow_uuid):
         self.flow_uuid = flow_uuid
+        self.flow = None
 
     def resolve(self):
-        return self.select_json(self.flow_uuid)[1]
+        if self.flow is None:
+            self.flow = self._select_flow(self.flow_uuid)
+        return self.flow.flow_data
 
-    def select_json(self, flow_uuid):
+    def resolve_label(self):
+        if self.flow is None:
+            self.flow = self._select_flow(self.flow_uuid)
+        return self.flow.label
+
+    def _select_flow(self, flow_uuid):
         """
         uuidを受け取って、json文字列を返す
         """
