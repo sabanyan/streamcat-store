a1e56ee968eb9d141a4c6b50e3c11389dfb238fb
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2019-09-23 12:37:15 +0900

    Fix updating path along with label

--- kskp/store/awss3.py
+++ kskp/store/awss3.py
@@ -109,7 +109,8 @@ class AwsS3(Folder):
 
         try:
             # ディレクトリ名の移動によって他のDatumのpathが変更が必要であれば変更する
-            Datum.update_all_path(old_path, new_path, modifier)
+            Datum.update_same_path(old_path, new_path, modifier)
+            Datum.update_include_path(old_path, new_path, modifier)
 
             # レコードを更新する
             data = {'label' : new_label, 'bucket' : bucket_name}
