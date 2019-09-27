1e834dd3d483ead02a137ff8d9460caf8e36bc6f
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2019-09-27 09:28:03 +0900

    Fix created_at time in library to accurate time

--- kskp/store/store_model.py
+++ kskp/store/store_model.py
@@ -23,8 +23,8 @@ class Store(BaseModel):
     data        = Column(JSONB)
     creator     = Column(INTEGER)
     modifier    = Column(INTEGER)
-    created_at  = Column(TIMESTAMP, default=text('CURRENT_TIMESTAMP'))
-    modified_at = Column(TIMESTAMP, default=text('CURRENT_TIMESTAMP'), onupdate=text('CURRENT_TIMESTAMP'))
+    created_at  = Column(TIMESTAMP, default=text('statement_timestamp()'))
+    modified_at = Column(TIMESTAMP, default=text('statement_timestamp()'), onupdate=text('statement_timestamp()'))
 
     def __init__(self, id=None, data=None, creator=None):
         self.id = id
