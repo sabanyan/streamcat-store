7ac8f9e1514d2c3e04e194c4fe8d90b7449b28cb
hajaehoon <ha.jaehoon@ksk-anl.com>
2019-10-11 11:12:07 +0900

    graph

--- kskp/store/commands/visualizers/script.py
+++ kskp/store/commands/visualizers/script.py
@@ -292,3 +292,157 @@ class CsvToBoxplotCommand(VisualizersBokehPlot):
         plot=renderer.get_plot(boxwhisker).state
 
         return plot
+
+class CsvtoRepetitivieWaveform(VisualizersBokehPlot):
+
+    def __init__(self):
+        super().__init__()
+
+    def plot(self, args, inputs):
+        """
+        csvのファイルパスから、
+        plotの反復波形図を作成する
+        """
+        self.init(args, inputs)
+        dataset = self.get_dataset()
+        graph_sources = self.get_sources(dataset)
+        plot = self.get_layout("反復波形図",graph_sources)
+
+        return plot
+
+    def init(self, args, inputs):
+
+        # 共通パラメーター
+        frame_uuid = inputs.get('i')
+        # offset対応
+        offset = int(args.get('offset')) if args.get('offset') else 0
+        limit = int(args.get('limit')) if args.get('limit') else None
+        file_path = Library.load_frame(frame_uuid).path
+
+        self.graph_width = args.get('x_size')
+        self.graph_height = height=args.get('y_size')
+        self.x_axis_label = args.get('x_label')
+        self.y_axis_label = args.get('y_label')
+
+        # ブロック句
+        if not os.path.exists(file_path):
+            return ''
+
+        self.preprocess = args.get('*preprocess')
+        self.df = pd.read_csv(file_path, nrows=limit, skiprows=range(1, offset))
+
+        # 反復波形図用パラメーター    
+        self.column_name_id = args.get('ids')
+        self.column_name_attrs = args.get('attrs')
+        self.column_name_x_axis = args.get('x_axis')
+        self.column_name_values = args.get('y_axis')
+
+        # 共通設定
+        self.tools = "pan,wheel_zoom,box_zoom,reset,save,box_select"
+
+        # 反復波形図用設定
+        self.tooltips = [
+            (self.column_name_id, "@id"),
+            (self.column_name_attrs, "@attr"),
+            (self.column_name_x_axis, "@x"),
+            (self.column_name_values, "@y"),
+        ] 
+        # グラフ描画用データ
+        self.unique_df = self.df.drop_duplicates(self.column_name_id)
+        self.unique_ids = self.unique_df[self.column_name_id]
+        self.unique_attr = {}
+        for ui in self.unique_ids:
+            queryStr = '{0} == "{1}"'.format(self.column_name_id, ui)
+            self.unique_attr[ui] = self.unique_df.query(queryStr)[self.column_name_attrs].get_values()[0]
+        self.x_axis = self.df[self.column_name_x_axis].drop_duplicates().tolist()
+        self.dataset = self.get_dataset()
+        self.sources = self.get_sources(self.dataset)
+
+        self.filter = {self.column_name_id:[], self.column_name_attrs:''}
+
+    def get_dataset(self):
+        data = dict.fromkeys(self.unique_ids)
+        
+        for ui in data:
+            queryStr = '{0} == "{1}"'.format(self.column_name_id, ui)
+            data[ui] = {self.column_name_x_axis:dict.fromkeys(self.x_axis)}
+            data[ui][self.column_name_attrs] = self.unique_df.query(queryStr)[self.column_name_attrs].get_values()[0]
+            ui_df = self.df.query(queryStr)
+            for index, row in ui_df.iterrows():
+                x = row[self.column_name_x_axis]
+                y = row[self.column_name_values]
+                if x in data[ui][self.column_name_x_axis]:
+                    data[ui][self.column_name_x_axis][x] = y 
+
+        return data
+
+    def get_sources(self, dataset):
+        sources = dict.fromkeys(self.unique_df[self.column_name_id])
+        
+        for ui, attr in zip(self.unique_df[self.column_name_id], self.unique_df[self.column_name_attrs]):
+            dims = dict(filter(lambda item: item[1],dataset[ui][self.column_name_x_axis].items()))
+            dims = sorted(dims.items())        
+            x = [ item[0] for item in dims]
+            y = [ item[1] for item in dims]
+            sources[ui] = {}
+            sources[ui]['ColumnDataSource'] = ColumnDataSource(data=dict(
+                x = x,
+                y = y,
+                id = [ui] * len(x),
+                attr = [attr] * len(x)
+            ))
+            sources[ui][self.column_name_attrs] = self.unique_attr[ui]
+            
+        return sources
+
+    def get_layout(self, title, sources):
+        f = figure(title=title, tools=self.tools, tooltips=self.tooltips, width=self.graph_width, height=self.graph_height,x_axis_label=self.x_axis_label, y_axis_label=self.y_axis_label)
+        elements = {}
+        colors = itertools.cycle(palette)
+        for key, color in zip(sources,colors):
+            elements[key] = []
+            legend = '{0}({1})'.format(key, sources[key][self.column_name_attrs])
+            elements[key].append(f.line('x', 'y', source=sources[key]['ColumnDataSource'], legend=legend, color=color, alpha=0.75, muted_color=color, muted_alpha=0.2))
+            elements[key].append(f.circle('x', 'y', source=sources[key]['ColumnDataSource'], legend=legend, color=color, alpha=0.75, muted_color=color, muted_alpha=0.2))
+            
+        f.legend.location = "top_left"
+        f.legend.click_policy = "mute"
+
+        attrs = sorted(self.unique_df[self.column_name_attrs].unique().tolist())
+        attrs.insert(0, '-All-')
+        attr_select = Select(value=attrs[0], title=self.column_name_attrs, options=attrs) 
+        
+        callback = CustomJS(args=dict(sources=sources, elements=elements, select=attr_select, attrs_list=attrs, name_attrs=self.column_name_attrs), code="""
+                
+            var isMute = (id, attr, filtered) => {
+                return !(attr === filtered['selected_attr'])
+            }
+            var update = (key, mute) => { 
+                elements[key].map((graphElement) => {
+                    graphElement.muted = mute
+                })
+            }
+
+            var clear = (key) => { 
+                elements[key].map((graphElement) => {
+                    graphElement.muted = false
+                })
+            }
+
+            var value = select.value
+            var filtered = {'selected_attr':value}
+            for (key in sources) {
+                var source_id = key
+                var source_attr = sources[key][name_attrs]
+                var mute = isMute(key, source_attr, filtered)
+                if (value === attrs_list[0]) {
+                    clear(key)
+                } else {
+                    update(key, mute)
+                }
+            }
+        """)
+
+        attr_select.js_on_change('value', callback)
+
+        return gridplot([f, attr_select], ncols=1)
