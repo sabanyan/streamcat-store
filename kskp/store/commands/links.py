db850297f62dbb6f8f668c819ec9733b4698131d
Aldrich Suratos <aldrich.suratos@ksk-anl.com>
2019-10-11 12:51:47 +0900

    merged mvavg_fixed functionality into mvavg

--- kskp/store/commands/links.py
+++ kskp/store/commands/links.py
@@ -131,7 +131,6 @@ class CommandLink:
             'multimcal_manyformula': MultiMcalCommand(),
             'multimcal_oneformula': MultiMcalWCCommand(),
             'mvavg': MvAvgCommand(),
-            'mvavg_fixed': MvAvgFixedCommand(),
             'mvstats': MvStatsCommand(),
             'mvstats_fixed': MvStatsFixedCommand(),
             'selrow': SelRowCommand(),
