e9ae6ad528d4c5a46df62e879ba8697e07416729
cohyou <cohyou@ironoir.io>
2019-12-02 18:22:36 +0900

    change directoryname of STORE_DIR to 'depo'

--- kskp/store/__init__.py
+++ kskp/store/__init__.py
@@ -9,8 +9,7 @@ CACHE_FOLDER_UUID   = 'cc9f050d-b007-414e-a6e0-6d31a9c13395'
 CACHE_FOLDER_LABEL  = 'キャッシュ'
 
 # フローがDBに保存されるようになるまでは下記のパスをstoreが持っておく
-# STORE_DIR = Path(__file__).parent.parent / 'store'
-STORE_DIR = Path(__file__).parent.parent.parent / 'store'
+STORE_DIR = Path(__file__).parent.parent / 'depo'
 FLOW_PATH = (STORE_DIR / 'flows/json').as_posix()
 if not os.path.exists(FLOW_PATH):
     os.makedirs(FLOW_PATH)
