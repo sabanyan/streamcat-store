a602cc192fe3c109dba26b6ad229dfae9ce7c626
cohyou <cohyou@ironoir.io>
2019-12-10 18:41:00 +0900

    fix typo

--- kskp/store/folder.py
+++ kskp/store/folder.py
@@ -207,7 +207,7 @@ class Folder(Store):
         """
         try:
             # 同じ名称のファイルが既に存在する場合、末尾に数字を付加したディレクトリ名で作成する
-            if self.path.exists() and len(list(self.path.iterdir())) == 0
+            if self.path.exists() and len(list(self.path.iterdir())) == 0:
                 # すでにパスが存在していても、中身がなければ利用する
                 # depo対応のため
                 path = self._path
