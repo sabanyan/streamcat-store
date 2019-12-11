760d4a652beaa41e360567f0029da1d925b13ce2
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2019-12-11 15:57:21 +0900

    Fixed to get content property of inputs

--- kskp/depo/std/commands/pcmd/script.py
+++ kskp/depo/std/commands/pcmd/script.py
@@ -231,7 +231,7 @@ class WinCp932ReadCommand(PCommand):
 
     def run(self, args, inputs):
         f = None
-        f <<= inputs['i']
+        f <<= inputs['i'].content
 
         args_string = (PCMD_DIR / 'src/windows_cp932_csv_read.sh').as_posix()
         args_string += self.replace_args(args)
