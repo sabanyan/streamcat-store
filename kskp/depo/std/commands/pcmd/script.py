2793caa2ae588c0738a48546a1cf8ee2ad935769
Aldrich Suratos <aldrich.suratos@ksk-anl.com>
2019-12-11 15:23:20 +0900

    Fix multimcal arglist bug

--- kskp/depo/std/commands/pcmd/script.py
+++ kskp/depo/std/commands/pcmd/script.py
@@ -2754,6 +2754,8 @@ class MultiMcalCommand(Command):
             else:
                 cmd_o <<= nm.mcal({**acarg,**args})
 
+        args['arglist'] = aclist
+
         nysol_module_o= NysolModule()
         nysol_module_o.set_content(cmd_o)
         return {'o': nysol_module_o}
