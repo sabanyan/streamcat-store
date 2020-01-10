ffbddd2878079d8679e651f974d14ea0996f4eb7
cohyou <cohyou@ironoir.io>
2020-01-10 16:56:10 +0900

    fix revision

--- alembic/versions/88beadc294df_create_user_table.py
+++ alembic/versions/88beadc294df_create_user_table.py
@@ -30,35 +30,35 @@ def upgrade():
     )
     op.create_table(
         'users_groups',
-        sa.Column('user_id', String)
-        sa.Column('group_id', String)
-        sa.Column('creator', Integer)
-        sa.Column('modifier', Integer)
-        sa.Column('created_at', String, default=text('CURRENT_TIMESTAMP'))
-        sa.Column('modified_at', String, default=text('CURRENT_TIMESTAMP'))
+        sa.Column('user_id', String),
+        sa.Column('group_id', String),
+        sa.Column('creator', Integer),
+        sa.Column('modifier', Integer),
+        sa.Column('created_at', String, default=text('CURRENT_TIMESTAMP')),
+        sa.Column('modified_at', String, default=text('CURRENT_TIMESTAMP')),
     )
     op.create_table(
         'groups',
-        sa.Column('id', String, primary_key=True)
-        sa.Column('name', String, nullable=False)
-        sa.Column('is_admin', Integer, default=0, nullable=False)
-        sa.Column('creator', Integer)
-        sa.Column('modifier', Integer)
-        sa.Column('created_at', String, default=text('CURRENT_TIMESTAMP'))
-        sa.Column('modified_at', String, default=text('CURRENT_TIMESTAMP'))
+        sa.Column('id', String, primary_key=True),
+        sa.Column('name', String, nullable=False),
+        sa.Column('is_admin', Integer, default=0, nullable=False),
+        sa.Column('creator', Integer),
+        sa.Column('modifier', Integer),
+        sa.Column('created_at', String, default=text('CURRENT_TIMESTAMP')),
+        sa.Column('modified_at', String, default=text('CURRENT_TIMESTAMP')),
     )
     op.create_table(
         'auth',
-        sa.Column('group_id', String, primary_key=True)
-        sa.Column('data_id', String, primary_key=True)
-        sa.Column('read', Integer, default=0, nullable=False)
-        # write       = Column(Integer, default=0, nullable=False)
-        # exec        = Column(Integer, default=0, nullable=False)
-        # own         = Column(Integer, default=0, nullable=False)
-        sa.Column('creator', Integer)
-        sa.Column('modifier', Integer)
-        sa.Column('created_at', String, default=text('CURRENT_TIMESTAMP'))
-        sa.Column('modified_at', String, default=text('CURRENT_TIMESTAMP'))
+        sa.Column('group_id', String, primary_key=True),
+        sa.Column('data_id', String, primary_key=True),
+        sa.Column('read', Integer, default=0, nullable=False),
+        # sa.Column('write', Integer, default=0, nullable=False),
+        # sa.Column('exec', Integer, default=0, nullable=False),
+        # sa.Column('own', Integer, default=0, nullable=False),
+        sa.Column('creator', Integer),
+        sa.Column('modifier', Integer),
+        sa.Column('created_at', String, default=text('CURRENT_TIMESTAMP')),
+        sa.Column('modified_at', String, default=text('CURRENT_TIMESTAMP')),
     )
 
 
