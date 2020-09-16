8f8e39366748cdf028a5ff3225ff590848456e48
Aldrich Suratos <aldrich.suratos@ksk-anl.com>
2020-09-16 14:13:17 +0900

    fix command id links

--- kskp/depo/std/commands/pcmd/groupby_remake.py
+++ kskp/depo/std/commands/pcmd/groupby_remake.py
@@ -741,6 +741,11 @@ class GroupByRemakeCommand(PCommand):
         if ks:
             ks_list = ks.split(',')
             
+            for key in ks_list:
+                if key not in self.header:
+                    errmsg = self.generateCommandErrorMessage('FieldNotFoundError', 'k', key)
+                    raise Exception(errmsg)
+                    
             if self.containsAny(ks, '%&'):
                 errmsg = self.generateCommandErrorMessage('KeyFieldForbiddenCharacterError', 'k', ks)
                 raise Exception(errmsg)
