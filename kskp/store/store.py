a4fb73f57c1b81accd84f5be92f39dce54b77400
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-09-25 07:20:54 +0900

    Not grant permissions to data everyone at viewing the data

--- kskp/store/store.py
+++ kskp/store/store.py
@@ -24,15 +24,15 @@ class Store(Datum):
         # DatumについてEveryOneロールの権限設定がない場合、初期値を設定する
         # (後方互換、一覧表示の速度を結構遅くしている)
         # 
-        for datum in data:
-            from kskp.store.factory import RoleFactory, AuthFactory
-            everyone_role = RoleFactory(self._session).load_everyone_role()
-            everyone_role.join_user(self._session.user)
-            if not AuthFactory(self._session).exists(everyone_role.id, datum.id):
-                from kskp.store import Folder, Flow
-                # FolderまたはFlowの場合は実行権限を付与する
-                folder_or_flow = isinstance(datum, Folder) or isinstance(datum, Flow) or None
-                everyone_role.init_authz(datum.id, True, True, exec=folder_or_flow)
+        # for datum in data:
+        #     from kskp.store.factory import RoleFactory, AuthFactory
+        #     everyone_role = RoleFactory(self._session).load_everyone_role()
+        #     everyone_role.join_user(self._session.user)
+        #     if not AuthFactory(self._session).exists(everyone_role.id, datum.id):
+        #         from kskp.store import Folder, Flow
+        #         # FolderまたはFlowの場合は実行権限を付与する
+        #         folder_or_flow = isinstance(datum, Folder) or isinstance(datum, Flow) or None
+        #         everyone_role.init_authz(datum.id, True, True, exec=folder_or_flow)
 
         return data
 
