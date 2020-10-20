669eba64f9ccd78e4dc5ddd2521853da2df2ed92
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-10-20 20:42:22 +0900

    Add setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='kskp.store',
     packages=['kskp.store'],
+    description='Data Store of many type',
+    url='https://www.ksk-anl.com/products/kskp',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
     install_requires=[
         'ordered_set',
         'chardet',
