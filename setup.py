028548adf97802c4c9b39edd3f4fa521512e26f0
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-10-21 14:31:59 +0900

    Gave up to update version in setup.py automatically

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ setup(
     packages=['kskp.store'],
     description='Data Store of many type',
     url='https://www.ksk-anl.com/products/kskp',
-    version_config={
-        "version_format": '{tag}',
-        "starting_version": '2.0.0'
-    },
-    setup_requires=['better-setuptools-git-version'],
     install_requires=[
         'ordered_set',
         'chardet',
