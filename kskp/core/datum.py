dcfeae08d0678171375fd21c2084e6d3b43eb941
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-10-21 14:38:31 +0900

    Fixed sort order so that folders are followed after projects

--- kskp/core/datum.py
+++ kskp/core/datum.py
@@ -44,16 +44,16 @@ class Datum(BaseModel):
     # ルートフォルダのPath
     DEFAULT_LIBRARY_PATH = Path('cmn')
 
-    FOLDER_TYPE = 'folder'
     PROJECT_TYPE = 'project'
+    FOLDER_TYPE = 'folder'
     AWSS3_TYPE  = 'awss3'
     RFOLDER_TYPE = 'rfolder'
     DATABASE_TYPE = 'database'
     FLOW_TYPE   = 'flow'
     FRAME_TYPE  = 'frame'
-    TRASH_TYPE = 'trash'
     COMMAND_TYPE = 'command'
     ACTIVITY_TYPE = 'activity'
+    TRASH_TYPE = 'trash'
 
     RESULT_FOLDER_UUID  = 'aacb4914-0695-40fc-b14b-95b7f1f81707'
     RESULT_FOLDER_LABEL = '実行結果'
@@ -80,7 +80,17 @@ class Datum(BaseModel):
     _path        = Column('path', PathType, nullable=False)
     _label       = Column('label', String)
     # PostgreSQLのENUM型の要素を変更してもSQLAlchemyから自動的に変更がかからないので手動で変更する必要がある
-    type         = Column(ENUM(FOLDER_TYPE, PROJECT_TYPE, AWSS3_TYPE, RFOLDER_TYPE, DATABASE_TYPE, FLOW_TYPE, FRAME_TYPE, TRASH_TYPE, COMMAND_TYPE, ACTIVITY_TYPE, name='data_type'), nullable=False)
+    type         = Column(ENUM( PROJECT_TYPE,
+                                FOLDER_TYPE,
+                                AWSS3_TYPE,
+                                RFOLDER_TYPE,
+                                DATABASE_TYPE,
+                                FLOW_TYPE,
+                                FRAME_TYPE,
+                                COMMAND_TYPE,
+                                ACTIVITY_TYPE,
+                                TRASH_TYPE,
+                                name='data_type'), nullable=False)
     _data        = Column('data', JSONB)
     _creator_id  = Column('creator', INTEGER)
     _modifier_id = Column('modifier', INTEGER)
