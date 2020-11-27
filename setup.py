325e1c29b3f825be220eb58f72ebad30f1659de5
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-11-27 18:32:39 +0900

    Engraved Version number

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='kskp.store',
     packages=['kskp.store'],
+    version='3.0',
     description='Data Store of many type',
     url='https://www.kskp.io',
     install_requires=[
