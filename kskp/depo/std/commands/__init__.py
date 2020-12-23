2537918f111ad8e235178b7c9fcd0756364777a1
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-12-24 00:18:22 +0900

    Removed unused visualizers commands

--- kskp/depo/std/commands/__init__.py
+++ kskp/depo/std/commands/__init__.py
@@ -4,7 +4,6 @@ from .kcmd.script import *
 from .pcmd.script import *
 from .scmd.script import *
 from .vcmd.script import *
-from .visualizers.script import *
 
 # linksのimport
 from .links import CommandLink, CommandsPathLink, CommandsPathFileSource
