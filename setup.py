d45dbea4973cf8d9f3850466c436d7a9a30b9898
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-12-24 00:37:33 +0900

    Fixed to add matplotlib

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(
         'scipy',
         'pandas==0.24.2',
         'sklearn',
+        # matplotlibはholoviewsが使用する
+        'matplotlib',
         'holoviews==1.12.7',
         # param 1.10.0では以下のWarningが多量に表示される、bokehが参照している?
         # "WARNING:param.Dimension: Use method 'get_param_values' via param namespace"
