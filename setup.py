d4861d7f5d82b3f636993e1e34e434baa25676ed
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2021-03-22 19:59:51 +0900

    Update bokeh version to 2.3.0

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setup(
         # param 1.10.0では以下のWarningが多量に表示される、bokehが参照している?
         # "WARNING:param.Dimension: Use method 'get_param_values' via param namespace"
         'param<=1.9.3',
-        'bokeh==1.4.0',
+        'bokeh==2.3.0',
     ],
 )
