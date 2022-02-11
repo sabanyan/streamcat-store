2dfa07cca3f9ea11efd3c6435195536e55220f9b
hiroshi80 <it.gumbler@gmail.com>
2022-02-11 18:55:10 +0900

    Miscellaneous renames

--- streamcat/store/remote_folder_conn.py
+++ streamcat/store/remote_folder_conn.py
@@ -59,7 +59,7 @@ class RemoteFolderConn():
         import sys
         if self.protocol == 'smb':
             if sys.platform == 'linux':
-                return f'sudo mount -t cifs -o "user={self.user_id},pass={self.password},domain={self.domain},uid=scat,gid=scat" ' +\
+                return f'sudo mount -t cifs -o "user={self.user_id},pass={self.password},domain={self.domain},uid=streamcat,gid=streamcat" ' +\
                        f'"//{self.hostname}/{self.directory}" "{mount_point_path.as_posix()}"'    
             elif sys.platform == 'darwin':
                 # macOS
