e2d065f3c34cf5e3d675187ea9a5ea2ebb14989b
hiroshi80 <it.gumbler@gmail.com>
2023-02-21 12:00:23 +0900

    Incremented version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='streamcat.store',
     packages=['streamcat.store'],
-    version='3.2',
+    version='3.3',
     description='Data Store of many type',
     url='https://www.kskp.io',
     install_requires=[
